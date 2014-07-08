8b553b17a02424f857a5a400a3b80dff0ec862d9
Lindsey Menges <lindseymenges@gmail.com>
2014-07-08 10:53:30 -0700

    creation of public usernames functional. TODO: sessions

--- anonymousjournaling/app/controllers/users_controller.rb
+++ anonymousjournaling/app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
+  def create_public_username
+    full_public_username = Faker::Commerce.product_name + " " + Random.rand(999).to_s
+    if User.find_by_public_username(full_public_username) == nil
+      return full_public_username
+    else
+      create_public_username
+    end
+  end
 
   def index
   end
@@ -12,7 +20,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(params[:user])
-    @user.public_username = "test_user"
+    @user.public_username = create_public_username
     if @user.save
       session[:user_id] = @user.id
       redirect_to root_path
